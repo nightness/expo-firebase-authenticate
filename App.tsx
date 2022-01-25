e2f38720a07d062a2d84a3607dc1f529b779a5e3
Josh Guyette <joshguyette@yahoo.com>
2022-01-24 20:40:53 -0600

    clean-up and source comments

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
+/*
+	This app consists of two screens, each with one button (either 'Login to Google' or 'Logout')
+*/
 import React, { useEffect, useState } from 'react';
 import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import { maybeCompleteAuthSession } from 'expo-web-browser';
-import { FirebaseApp, getApp, getApps, initializeApp } from 'firebase/app';
+import { FirebaseApp, getApp, getApps, initializeApp, FirebaseError } from 'firebase/app';
 import { useAuthRequest, useIdTokenAuthRequest } from 'expo-auth-session/providers/google';
 import { getAuth, GoogleAuthProvider, signInWithCredential, signInWithRedirect, signOut } from 'firebase/auth';
-
 import Constants from 'expo-constants';
-
-import { FirebaseError } from 'firebase/app';
 
 // If you want to setup your own firebase test project, all config is in here
 import { authOptions, clientIds, firebaseConfig, SCHEME } from './config';
 
-// maybeCompleteAuthSession({
-// 	skipRedirectCheck: true,
-// });
+// The styles for components
+import { styles } from './styles';
 
 // Initialize Firebase
 let firebaseApp: FirebaseApp;
@@ -24,6 +22,10 @@ if (getApps().length === 0) {
 	firebaseApp = getApp();
 }
 
+//
+//	This is the LoginPage Component
+//		onLogin: Happens have firebase has a current user
+//
 const LoginPage = ({ onLogin }: any) => {
 	const auth = getAuth();
 
@@ -38,17 +40,14 @@ const LoginPage = ({ onLogin }: any) => {
 
 	// Logging, this is the request we send to google
 	useEffect(() => {
-		console.log(`App.tsx: useEffect (requestToGoogle) requestToGoogle: ${JSON.stringify(requestToGoogle)}`);
+		console.log(`LoginPage: useEffect (requestToGoogle): ${JSON.stringify(requestToGoogle)}`);
 	}, [requestToGoogle]);
 
 	// Handles the response from google authentication (iOS / Android / Expo Go)
 	useEffect(() => {
+		// Successful response
 		if (responseFromGoogle?.type === 'success') {
-			console.log(
-				`App.tsx: useEffect (responseFromGoogle)  responseFromGoogle?.type === 'success' responseFromGoogle: ${JSON.stringify(
-					responseFromGoogle
-				)}`
-			);
+			console.log(`LoginPage: useEffect (responseFromGoogle): ${JSON.stringify(responseFromGoogle)}`);
 
 			const { id_token } = responseFromGoogle.params;
 			const auth = getAuth();
@@ -56,24 +55,21 @@ const LoginPage = ({ onLogin }: any) => {
 			signInWithCredential(auth, credential)
 				.then((value) => {
 					console.log(
-						`App.tsx: useEffect (responseFromGoogle)  signInWithCredential(auth, credential) ${JSON.stringify(
+						`LoginPage: useEffect (responseFromGoogle)  signInWithCredential(auth, credential) ${JSON.stringify(
 							value
 						)} going to onLogin with ${JSON.stringify(value.user)}`
 					);
-					// console.log('AuthenticationPage: Successful native login using Google');
 					onLogin(value.user);
 				})
 				.catch((err) => {
-					console.log(`App.tsx: useEffect (responseFromGoogle)  ERROR ERROR ERROR`);
-					// alert(JSON.stringify(err));
+					console.log(`LoginPage: useEffect (responseFromGoogle)  ERROR ERROR ERROR`);
 				});
-		} else {
+		}
+		// THIS IS THE POINT OF WHERE IT FAILS!!!!!
+		else {
 			console.log(
-				`App.tsx: LoginPage useEffect (responseFromGoogle) responseFromGoogle?.type  ${JSON.stringify(
-					responseFromGoogle,
-					null,
-					2
-				)}`
+				`LoginPage: useEffect (responseFromGoogle)
+				${JSON.stringify(responseFromGoogle, null, 2)}`
 			);
 		}
 	}, [responseFromGoogle]);
@@ -86,76 +82,71 @@ const LoginPage = ({ onLogin }: any) => {
 		}
 	}, [auth]);
 
-	// Platform independent sign-in with google
-	const signInWithGoogle = async () => {
-		if (Platform.OS === 'web') {
-			console.log(`App.tsx: signInWithGoogle(): Platform Web`);
-			const provider = new GoogleAuthProvider();
-			signInWithRedirect(getAuth(), provider)
-				.then(() => {
-					onLogin(auth.currentUser);
-				})
-				.catch((error: FirebaseError) => {
-					console.error(error);
-				});
-		} else if (Platform.OS === 'android' || Platform.OS === 'ios') {
-			console.log(`App.tsx: signInWithGoogle(): Platform NOT Web`);
-			promptGoogleAuthAsync();
-		}
-	};
-
-	console.log(`App.tsx: LoginPage Rendering`);
-
+	// Render component
 	return (
-		<TouchableOpacity style={styles.touchable} onPress={signInWithGoogle}>
+		<TouchableOpacity
+			style={styles.touchable}
+			onPress={() => {
+				console.log(`LoginPage: signInWithGoogle()`);
+				promptGoogleAuthAsync();
+			}}
+		>
 			<Text style={{ fontSize: 20, fontWeight: '700' }}>Login with Google</Text>
 		</TouchableOpacity>
 	);
 };
 
+//
+//	This is the LogoutPage Component
+//		onLogout: Happens after firebase current user is null
+//
 const LogoutPage = ({ onLogout }: any) => {
 	const auth = getAuth();
 
+	// Logs out of firebase, with success and error callbacks
 	const logout = (onSuccess: () => any, onError?: (error: any) => any) => {
 		let localAuth = getAuth();
 
 		signOut(localAuth)
 			.then(() => {
-				console.log(`FirebaseContext: WE ARE IN THE SIGNOUT >>>>>>>`);
+				console.log(`Logout: SignOut results`);
 				try {
 					onSuccess?.();
 				} catch (err) {
-					console.log(`FirebaseContext: ERROR in setGuest() >>>>>>> ${JSON.stringify(err)}`);
+					console.log(`Logout: ERROR in setGuest() >>>>>>> ${JSON.stringify(err)}`);
 					onError && onError(err as any);
 				}
-				// onSuccess();
 			})
 			.catch((err) => {
-				console.error([`FirebaseContext: ERROR LOGGING OUT>>>>>`, err]);
+				console.error([`Logout: ERROR LOGGING OUT>>>>>`, err]);
 				onError && onError(err);
 			});
 	};
 
+	// When there is no current user, we are logged out, so call onLogout callback
 	useEffect(() => {
 		if (!auth.currentUser) {
-			console.log(`App.tsx: Logged out... auth.currentUser: ${JSON.stringify(auth.currentUser)}`);
+			console.log(`LogoutPage: currentUser = ${JSON.stringify(auth.currentUser)} (null is good)`);
 			onLogout();
 		}
 	}, [auth]);
 
-	const onPressHandler = () => {
-		logout(onLogout, () => console.error);
-	};
-
-	console.log(`App.tsx: LogoutPage(): Rendering LogoutPage`);
-
+	// Render component
 	return (
-		<TouchableOpacity style={styles.touchable} onPress={onPressHandler}>
+		<TouchableOpacity
+			style={styles.touchable}
+			onPress={() => {
+				logout(onLogout, () => console.error);
+			}}
+		>
 			<Text style={{ fontSize: 20, fontWeight: '700' }}>Logout</Text>
 		</TouchableOpacity>
 	);
 };
 
+///
+/// App Component
+///
 export default function App() {
 	const [loggedIn, setLoggedIn] = useState(false);
 
@@ -179,25 +170,3 @@ export default function App() {
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignContent: 'center',
-		alignSelf: 'center',
-		maxHeight: 70,
-		justifyContent: 'center',
-		borderColor: 'black',
-		borderRadius: 10,
-		borderWidth: 5,
-		marginTop: 100,
-	},
-	touchable: {
-		alignContent: 'center',
-		alignSelf: 'center',
-		justifyContent: 'center',
-		width: '100%',
-		height: '100%',
-		paddingHorizontal: 20,
-	},
-});
---
+++ styles.ts
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignContent: 'center',
+		alignSelf: 'center',
+		maxHeight: 70,
+		justifyContent: 'center',
+		borderColor: 'black',
+		borderRadius: 10,
+		borderWidth: 5,
+		marginTop: 100,
+	},
+	touchable: {
+		alignContent: 'center',
+		alignSelf: 'center',
+		justifyContent: 'center',
+		width: '100%',
+		height: '100%',
+		paddingHorizontal: 20,
+	},
+});
